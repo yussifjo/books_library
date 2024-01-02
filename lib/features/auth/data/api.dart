bc4c5310bc0aca39c6804cc100f6528a96ae34fb
yussifjo <yussifjo7@gmail.com>
2024-01-02 19:18:55 +0200

    api

---
+++ lib/features/auth/data/api.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ApiConection extends StatelessWidget {
+  const ApiConection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
